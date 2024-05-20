3adc6cd92281839d3ec20352757063c0e54dfc5e
Walid Kabboussa <walidkabou@gmail.com>
2024-05-20 23:59:36 +0100

    Setup express and  read data from json file

---
+++ Backend/server.js
@@ -0,0 +1,35 @@
+import express from 'express';
+import fs from 'fs';
+import cors from 'cors';
+
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+let data;
+try {
+  data = JSON.parse(fs.readFileSync('properties.json', 'utf-8'));
+} catch (err) {
+  console.error('Error reading data.json:', err);
+  process.exit(1); // 
+}
+
+
+app.get('/api/properties', (req, res) => {
+  res.json(data.properties);
+});
+
+app.get('/api/properties/:id', (req, res) => {
+  const { id } = req.params;
+
+  const property = data.properties.find(prop => prop._id === id);
+  if (property) {
+    res.json(property);
+  } else {
+    res.status(404).send('Property not found');
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
